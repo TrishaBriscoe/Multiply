2b910c008efd4bd3092fa6ec0b3a8a69b514a490
Trisha Briscoe <tbris225@d127.org>
2022-11-22 08:47:49 -0600

    Initial Commit

--- Multiply/ViewController.swift
+++ Multiply/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var number2TextField: UITextField!
+    @IBOutlet weak var number1TextField: UITextField!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func calculateWhenPressed(_ sender: UIButton) {
+        //MVP - Trisha
+        
+        
+        //Stretch 2 & 3 - Katherine
+        
+        
+        //Stretch 1 - Ethan
+        
+    }
+    
+    
+    
+    
 
 }
 
